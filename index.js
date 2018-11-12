cf2ee22041e4a556090955aa4b38b120a4446f43
Latif Sulistyo <latifsulistyo.me@gmail.com>
2018-11-12 12:53:50 +0700

    patch: function

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ module.exports = {
   extends: ['eslint-config-airbnb'].map(require.resolve),
   rules: {
     'no-console': 'off',
+    'func-names': ['error', 'as-needed'],
     'arrow-parens': ['error', 'as-needed'],
     'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': ['warn', 'never'],
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
     'object-curly-newline': ['error', { multiline: true }],
   },
