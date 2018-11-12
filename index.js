6be7d6db1ad8ebea4f851c94d55fa518c59070b9
Latif Sulistyo <latifsulistyo.me@gmail.com>
2018-11-12 12:11:48 +0700

    patch: allow underscore-dangle on _id used by mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
   extends: ['eslint-config-airbnb'].map(require.resolve),
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
     'no-console': 'off',
-    'object-curly-newline': ['error', { multiline: true }],
     'arrow-parens': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'object-curly-newline': ['error', { multiline: true }],
   },
   env: {
     browser: true,
