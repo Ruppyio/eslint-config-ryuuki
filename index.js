56f7b54c94f5127c4e129d80586c61ba52010d57
Latif Sulistyo <latifsulistyo.me@gmail.com>
2018-11-19 13:43:53 +0700

    patch: disable consistence-return

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ module.exports = {
   extends: ['eslint-config-airbnb'].map(require.resolve),
   rules: {
     'no-console': 'off',
+    'consistent-return': 'off',
     'func-names': ['warn', 'never'],
     'arrow-parens': ['error', 'as-needed'],
     'comma-dangle': ['error', 'always-multiline'],
     'space-before-function-paren': ['warn', 'never'],
-    'no-underscore-dangle': ['error', { allow: ['_id', '_doc'] }],
     'object-curly-newline': ['error', { multiline: true }],
+    'no-underscore-dangle': ['error', { allow: ['_id', '_doc'] }],
   },
   env: {
     browser: true,
